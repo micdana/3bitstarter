5b0dd8201f45c95c2d395ab76d22050fe20c8e80
micdana <1020.michael@gmail.com>
2015-05-28 01:23:36 +0000

    one more time5

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var buf = new Buffer();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    fs.readFile('/3bitstarter/index.html', function (err, data) {
-	if (err) throw err;
-    console.log(data);
-	});
-});
+    var read_all = buf.write(fs.readFileSync('/3bitstarter/index.html', 'utf8'));
+    var data_buf = buf.toString('utf8',0, read_all);
+response.send(data_buf);
+    }
+  );
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
