c3489e9339e3d8138fb979a70dbb792441ec0990
micdana <1020.michael@gmail.com>
2015-05-28 01:52:19 +0000

    another 7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var app = express.createServer(express.logger());
 var fs = require('fs');
 var read_myfile = fs.readFileSync('index/html','utf-8');
 
