c73c4914219755e97c5d706e428bf3277f1b71a3
micdana <1020.michael@gmail.com>
2015-05-28 01:57:17 +0000

    another 9

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var read_myfile = fs.readFileSync('index.html','utf-8');
+var myfile = fs.readFileSync('index.html','utf-8');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    response.send(read_myfile);
+    response.send(myfile);
     });
 
 app.listen(app.get('port'), function() {
