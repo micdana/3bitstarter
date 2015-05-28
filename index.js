40a11ffa4be911270ad3a70586f8379c4212212e
micdana <1020.michael@gmail.com>
2015-05-28 01:43:36 +0000

    another 6

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var buf = new Buffer();
+var read_myfile = fs.readFileSync('index/html','utf-8');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    var read_all = buf.write(fs.readFileSync('/3bitstarter/index.html', 'utf8'));
-    var data_buf = buf.toString('utf8',0, read_all);
-response.send(data_buf);
-    }
-  );
+    response.send(read_myfile);
+    });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
